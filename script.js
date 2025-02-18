2f11bb06ae7d7f50cc9eab52376b9377e58b2482
santiago calderon lara <santiagocalderonlara12@gmail.com>
2025-02-18 08:48:50 -0500

    primeros ejercicios en javascript con lugares donde se puede escribir

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-alert("no mames")
+
+// ejercicio 1
+let nombre = "Santiago calderon";
+let edad = 20;
+let ciudad = "Garzon";
+
+console.log("Hola, mi nombre es " + nombre + ", tengo " + edad + " años y vivo en " + ciudad + ".");
+
+// ejercicio 2
+/*crea una variable llamada precio y asignale un valor deciamal. luego crea otra variable llmada cantidad y asignale un 
+valor entero. Muestra el tipo de variable en la consola */
+
+
+let precio = 2.25;
+let cantidad = 3;
+//escribir en una consola
+console.log(typeof precio);
+console.log(typeof cantidad);
+//escrbir en una alerta
+alert(typeof precio);
+//escribir en un documento
+document.write(typeof cantidad);
+
+
